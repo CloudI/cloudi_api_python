d029670d3a80e513690bf24d7902b5676322e9f2
Michael Truog <mjtruog@gmail.com>
2017-09-12 23:25:21 -0700

    Version 1.7.2.

--- setup.py
+++ setup.py
@@ -46,10 +46,10 @@ setup(
         'Topic :: System :: Clustering',
         'Topic :: System :: Distributed Computing',
     ],
-    version='1.7.1',
+    version='1.7.2',
     description='Python CloudI API',
     author='Michael Truog',
     author_email='mjtruog@gmail.com',
     url='http://cloudi.org',
-    install_requires=['erlang_py==1.7.1'],
+    install_requires=['erlang_py==1.7.2'],
 )
